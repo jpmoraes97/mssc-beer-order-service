71ac5c9c892444e12ffb3b28e248513be21b188f
jpmoraes97 <lookinhoo@hotmail.com>
2020-05-07 18:48:26 -0300

    Enhancing order line with beer data. closes #3

---
+++ mssc-beer-order-service/src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -0,0 +1,36 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Service
+public class BeerServiceImpl implements BeerService {
+    public final String BEER_PATH_v1 = "/api/v1/beer";
+    private final RestTemplate restTemplate;
+    private String beerServiceHost;
+
+    public BeerServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID uuid) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_v1 + "/" + uuid.toString(), BeerDto.class));
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerbyUpc(String upc) {
+        return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_v1 + "/upc/" + upc, BeerDto.class));
+    }
+
+    public void setBeerServiceHost(String beerServiceHost) {
+        this.beerServiceHost = beerServiceHost;
+    }
+}
